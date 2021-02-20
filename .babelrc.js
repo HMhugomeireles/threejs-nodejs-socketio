c99ee9f348736b32d681e0bc3b2609422e1d6c49
Hugo Meireles <meireles.hmm@gmail.com>
2021-02-20 16:46:41 +0000

    setting up the webapck and babel config

---
+++ .babelrc.js
@@ -0,0 +1,9 @@
+const isProd = String(process.env.NODE_ENV) === 'production'
+const isTest = String(process.env.NODE_ENV) === 'test'
+
+module.exports = {
+  presets: [
+    ['@babel/preset-env', {modules: isTest ? 'commonjs' : false}]
+  ],
+  plugins: [],
+}
